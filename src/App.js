359cb8f9e98e9d7d385cf2dc75c41b81326a3e0a
Unique-Ade <olagbenrogabriel@gmail.com>
2024-04-26 15:17:48 +0100

    search bar initials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+
+      <div className="search">
+      <SearchBar/>
     </div>
+    </div>
+    
   );
 }
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,7 +1,7 @@
 const SearchBar = () => {
     return ( 
         <div className="search_bar">
-            
+            <input type="search" name="search-box" id="search" />
         </div>
      );
 }
