d3ec53ceb6625345a3a41d8075e786f5c6278306
Unique-Ade <olagbenrogabriel@gmail.com>
2024-04-26 18:38:19 +0100

    set search input value

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import search_icon from './Assets/search.svg';
 const SearchBar = () => {
+    const [input, setInput] = useState("");
     return ( 
         <div className="search_bar">
             <input type="search" name="search-box" id="search" placeholder="Search e.g C#, Js, PHP, JavaScript..." />
-            <img src={search_icon} alt=""/>
+            <img src={search_icon} alt="" value={input} onChange={e=>setInput(e.target.value)}/>
         </div>
      );
 }
