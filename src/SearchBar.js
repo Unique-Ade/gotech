6538042c363e44e550a5f67aa51bd53fd88a3d7d
Unique-Ade <olagbenrogabriel@gmail.com>
2024-04-26 15:53:18 +0100

    updated search style

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,7 +1,9 @@
+import search_icon from './Assets/search.svg';
 const SearchBar = () => {
     return ( 
         <div className="search_bar">
-            <input type="search" name="search-box" id="search" />
+            <input type="search" name="search-box" id="search" placeholder="Search e.g C#, Js, PHP, JavaScript..." />
+            <img src={search_icon} alt=""/>
         </div>
      );
 }
