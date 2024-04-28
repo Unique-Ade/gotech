365b593ddbfcfd31ca32115c50ab9ccffb8b78f0
Unique-Ade <olagbenrogabriel@gmail.com>
2024-04-28 19:47:33 +0100

    styled filtered tech words list

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,13 +9,14 @@ const SearchBar = () => {
     const { data: words, error } = UseFetch('http://localhost:5000/words');
     const [wordList, setWordList] = useState([]);
     const searchFilter = (e) => {
-        setWordList(words.filter((word) => word.name.toLowerCase().toUpperCase().includes(e.target.value)));
+        setWordList(words.filter((word) => word.name.toLowerCase().includes(e.target.value) 
+        || word.name.toUpperCase().includes(e.target.value)));
     }
     return (
         <div className="search_bar_main">
             <div className="search_bar">
                 <input type="search" name="search-box" id="search" placeholder="Search e.g C#, Js, PHP, JavaScript..."
-                  onChange={searchFilter} />
+                    onChange={searchFilter} />
                 <img src={search_icon} alt="" onClick={searchFilter} />
             </div>
             <TechWordsList wordList={wordList} />
