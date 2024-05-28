f2ec4fbce5df829679b4d7263e046126d0eaccc4
Unique-Ade <olagbenrogabriel@gmail.com>
2024-05-28 17:31:06 +0100

    styled registration form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Registration from "./Authentication/Registration";
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="index_body">
-      <Home/>
-    </div>
-    </div>
-    
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <div className="index_body">
+            <Route path="">
+              <Registration />
+            </Route>
+
+          </div>
+        </Switch>
+
+      </div>
+
+    </Router>
+
   );
 }
 
--- src/Authentication/Registration.jsx
+++ src/Authentication/Registration.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+    <div className="registration">
+      <h2 id="create-account">Create Your Account</h2>
+      <div className="name">
+        <div>
+          <span>First Name</span> <b></b>
+          <input type="text" placeholder="Enter First Name"  id="signUpInputName"/>
+        </div>
+        <div>
+          <span>Last Name</span> <br />
+          <input type="text" placeholder="Enter Last Name" id="signUpInputName" />
+        </div>
+      </div>
+      <div className="email">
+          <span>Email</span> <br />
+          <input type="text" placeholder="Enter Email" id="signUpInput"/>
+        </div>
+        <div className="password">
+          <span>Password</span> <br />
 
-export default Registration
+          <input type="text" placeholder="Enter Password" id="signUpInput" />
+        </div>
+        <div>
+        <span>Confirm Password</span> <br />
+        <input type="text" placeholder="Confirm Password" id="signUpInput" />
+        </div>
+        <button className="signUpBtn">Signup Now</button>
+    </div>
+  );
+};
+
+export default Registration;
